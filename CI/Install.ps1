522851ec70d748b1ef13e1a35840a839f6ab22be
Laurent LIENHARD <laurent.lienhard@outlook.com>
2019-03-23 17:35:46 +0100

    commit

--- CI/Install.ps1
+++ CI/Install.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = "Stop";
 
 Write-Output "[INSTALL] Start"
 
-if (!(Get-Module -ListAvailable -Name Pester | Where-Object { $_.Version -eq '4.7.1' })) {
+if (!(Get-Module -ListAvailable -Name Pester | Where-Object { $_.Version -eq '4.7.3' })) {
     Write-Output "[INSTALL] Pester"
-    Install-Module -Name pester -MinimumVersion 4.7.1 -Scope CurrentUser -Force -Confirm:$false
+    Install-Module -Name pester -MinimumVersion 4.7.3 -Scope CurrentUser -Force -Confirm:$false -SkipPublisherCheck
 }
 
-if (!(Get-Module -ListAvailable -Name PlatyPS | Where-Object { $_.Version -eq '0.12.0' })) {
+if (!(Get-Module -ListAvailable -Name PlatyPS | Where-Object { $_.Version -eq '0.13.0' })) {
     Write-Output "[INSTALL] PlatyPS"
-    Install-Module -Name PlatyPS -MinimumVersion 0.12.0 -Scope CurrentUser -Force -Confirm:$false
+    Install-Module -Name PlatyPS -MinimumVersion 0.13.0 -Scope CurrentUser -Force -Confirm:$false
 }
 
 if (!(Get-Module -ListAvailable -Name PSDeploy | Where-Object { $_.Version -eq '1.0.1' })) {
@@ -22,16 +22,16 @@ if (!(Get-Module -ListAvailable -Name PSScriptAnalyzer | Where-Object { $_.Versi
     Install-Module -Name PSScriptAnalyzer -MinimumVersion 1.17.1 -Scope CurrentUser -Force -Confirm:$false
 }
 
-if (!(Get-Module -ListAvailable -Name PSClassUtils | Where-Object { $_.Version -eq '2.6.1' })) {
+if (!(Get-Module -ListAvailable -Name PSClassUtils | Where-Object { $_.Version -eq '2.6.3' })) {
     Write-Output "[INSTALL] PSClassUtils"
-    Install-Module -Name PSClassUtils -MinimumVersion 2.6.1 -Scope CurrentUser -Force -Confirm:$false
+    Install-Module -Name PSClassUtils -MinimumVersion 2.6.3 -Scope CurrentUser -Force -Confirm:$false
     Import-module PSClassUtils
-    Install-PackageProvider -Name Chocolatey -force;
-    Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -Force -Trusted;
-    Find-Package graphviz -Source "http://chocolatey.org/api/v2/"  | Install-Package -ForceBootstrap -Force;
-    Start-Sleep -Seconds 2;
+    Install-PackageProvider -Name Chocolatey -force -Scope CurrentUser
+    Register-PackageSource -Name Chocolatey -ProviderName Chocolatey -Location http://chocolatey.org/api/v2/ -Force -Trusted
+    Find-Package graphviz -Source "http://chocolatey.org/api/v2/"  | Install-Package -ForceBootstrap -Force
+    Start-Sleep -Seconds 2
 
-    Install-PackageProvider -Name NuGet -Force -Scope "CurrentUser";
-    Install-Module -Name PSGraph -Force -verbose -Scope "CurrentUser";
+    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+    Install-Module -Name PSGraph -Force -verbose -Scope CurrentUser
 }
 Write-Output "[INSTALL] End"
