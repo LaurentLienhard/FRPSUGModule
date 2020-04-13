75793ea664ecc1d35633cd377debc58bf0b7f668
Laurent LIENHARD <laurent.lienhard@outlook.com>
2020-04-13 17:31:21 +0200

    commit test

--- FRPSUGModule.Build.ps1
+++ FRPSUGModule.Build.ps1
@@ -12,3 +12,17 @@ task Test SetBuildVariable, build, ImportModule, UnitTests
 task Helpify SetBuildVariable, ImportModule, GenerateMarkdown, GenerateHelp
 
 Write-Verbose "[INVOKEBUILMODULE][END]"
+Write-Verbose "[INVOKEBUILMODULE][START]"
+Write-Verbose "[INVOKEBUILMODULE] Import common tasks ..."
+Get-ChildItem -Path $env:BHProjectPath\BuildTasks\*.Task.ps1 | ForEach-Object {
+    Write-Verbose "[INVOKEBUILMODULE] Importing $($_.FullName)"
+    . $_.FullName
+}
+
+task Default SetBuildVariable, Clean, BuildModule, BuildManifest, Analyze, UnitTests, GenerateMarkdown, GenerateHelp
+task Build SetBuildVariable, Clean, BuildModule, BuildManifest
+task PSAnalyse SetBuildVariable, Analyze
+task Test SetBuildVariable, build, ImportModule, UnitTests
+task Helpify SetBuildVariable, ImportModule, GenerateMarkdown, GenerateHelp
+
+Write-Verbose "[INVOKEBUILMODULE][END]"
